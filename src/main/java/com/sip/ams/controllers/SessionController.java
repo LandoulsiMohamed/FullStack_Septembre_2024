504e1416b96b5e72806dd900e7a45c3264b0a606
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-09-15 12:11:42 +0100

    list session avec fragments et bootstrap

--- src/main/java/com/sip/ams/controllers/SessionController.java
+++ src/main/java/com/sip/ams/controllers/SessionController.java
@@ -1,14 +1,35 @@
 package com.sip.ams.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sip.ams.entities.Session;
 
 @Controller
 @RequestMapping("/sessions")
 public class SessionController {
 	
-	@RequestMapping("/list")
-	public String listSession() {
+	public static List<Session> sessions;
+	
+	static {
+		Session s1 = new Session("OCA","oca.png","C'est une formation permettant aux stagiaires d'avoir un niveau confirmé en tant que développeur Java, ce niveau vous permet de passer l'examen Oracle 1Z0-808 et de se plonger dans les framework tel que Spring.");
+		Session s2 = new Session("OCP","ocp.png","Ce niveau professionnel permet d'avoir  les compétences nécessaires pour être un développer senior en Java et capable de passer les certifications (Java 8) 1Z0-809, (Java 11)  1Z0-819 et (Java 17)  1Z0-829, Pour démarrer ce niveau, il faut avoir la formation OCA 1Z0-808.");
+		Session s3 = new Session("FullStack","fullstack.png","C'est une formation 100% pratique assurée un formateur expert de plus de 15% d'expérience en développement web et sous la supervision de 3 coaches. L'objectif de cette formation est de permettre au stagiaire de maîtriser les 3 fameux frameworks Spring, Angular et React Native.");
+		sessions = new ArrayList<>();
+		sessions.add(s1);
+		sessions.add(s2);
+		sessions.add(s3);
+	}
+	
+	@GetMapping("/list")
+	public String listSession(Model model) {
+		
+		model.addAttribute("sessions", sessions);
 		
 		return "sessions/listSessions";
 	}
---
+++ src/main/java/com/sip/ams/entities/Session.java
@@ -0,0 +1,45 @@
+package com.sip.ams.entities;
+
+public class Session {
+	
+	private String titre;
+	private String logo;
+	private String description;
+	
+	public Session(String titre, String logo, String description) {
+		super();
+		this.titre = titre;
+		this.logo = logo;
+		this.description = description;
+	}
+	
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Session [titre=" + titre + ", logo=" + logo + ", description=" + description + "]";
+	}
+
+}
